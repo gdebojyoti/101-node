9c4dc7876205fdcc7f43edb74b4d6ff03c9bab27
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2026-09-23 16:15:58 +0530

    Fake node module created.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // require("./1-welcome");
 // require("./2-global-this");
 
+import bingbing from "bingbing";
 import { sum } from "./maths/index.js";
 
 try {
+  bingbing();
+
   console.log(sum(2, 4, 5));
-  console.log(sum(2));
-  console.log(sum(2, 4, 5));
+  // console.log(sum(2));
+  // console.log(sum(2, 4, 5));
 } catch (e) {
   console.log(e);
 }
